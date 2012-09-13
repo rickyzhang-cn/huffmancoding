f513054d2554496b810400ca5f481477a70e6d39
rickyzhang <whrickyzc@gmail.com>
2012-09-13 21:07:28 +0800

    some correction

--- huffmancoding.c
+++ huffmancoding.c
@@ -19,7 +19,7 @@ Huffcode hc[MAXSIZE];
 
 int n;  //编码字符的个数
 
-void init(Huffnode *htp,Huffcode *hcp)	{
+void init()	{
 	char ch;
 	int i=0;
 	FILE *fp=fopen("text/in.txt","r");
@@ -28,17 +28,18 @@ void init(Huffnode *htp,Huffcode *hcp)	{
 		exit(0);
 	}
 
-	while(ch=fgetc(fp)!=EOF)	{
+	ht[i].parent=ht[i].lchild=ht[i].rchild=-1;
+
+	while((ch=fgetc(fp))!=EOF)	{
 		if(ch=='\n')	{
 			i++;
-			htp[i].parent=htp[i].lchild=htp[i].rchild=-1;
-			htp[i].weight=0;
+			ht[i].parent=ht[i].lchild=ht[i].rchild=-1;
 		}
 		else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))	{
-			hcp[i].w=ch;
+			hc[i].w=ch;
 		}
 		else if(ch>='0'&&ch<='9')	{
-			htp[i].weight=htp[i].weight*10+ch-'0';
+			ht[i].weight=ht[i].weight*10+ch-'0';
 		}	
 	}
 	
@@ -48,20 +49,22 @@ void init(Huffnode *htp,Huffcode *hcp)	{
 		printf("can't close the file\n");
 		exit(0);
 	}
+	printf("the n is %d\n",n);
 }
 
-void createht(Huffnode *htp)	{
+void createht()	{
 	int mini,minj;
 	int k,s;
 	int f;
 	
 	for(k=n;k<=(2*n-1);k++)	{
+		printf("this is a test in for\n");
 		for(s=0;s<n;s++)	{
 			f=0;
-			if(htp[s].parent==-1)	{
+			if(ht[s].parent==-1)	{
 				if(f==0)  { mini=s; f++; }
 				else if(f==1) {
-					if(htp[s].weight<htp[mini].weight)  { 
+					if(ht[s].weight<ht[mini].weight)  { 
 						minj=mini; mini=s; 
 					} 
 					else 
@@ -69,42 +72,45 @@ void createht(Huffnode *htp)	{
 					f++;
 				}
 				else	{
-					if(htp[s].weight<htp[mini].weight)	{
+					if(ht[s].weight<ht[mini].weight)	{
 						minj=mini; mini=s;
 					}
-					else if(htp[s].weight<htp[minj].weight)	
+					else if(ht[s].weight<ht[minj].weight)	
 						minj=s;
 				} 
 			}
 		}
-	htp[n].weight=htp[mini].weight+htp[minj].weight;
-	htp[n].parent=-1;
-	htp[n].lchild=mini;
-	htp[n].rchild=minj;
+	ht[k].weight=(ht[mini].weight+ht[minj].weight);
+	ht[k].parent=-1;
+	ht[k].lchild=mini;
+	ht[k].rchild=minj;
 	
-	htp[mini].parent=htp[minj].parent=n;
+	ht[mini].parent=ht[minj].parent=k;
 	}
 }
 
 
-void huffcode(Huffnode *htp,Huffcode *hcp)	{
+void huffcode()	{
 	int i,j,t;
+	printf("this is a test in huffcode function\n");
 	int length;
 	for(i=0;i<n;i++)	{
 		j=i;
+	printf("this is a test in huffcode function\n");
 		length=0;
-		if(htp[j].parent!=-1)	{
-			t=htp[j].parent;
-			if(htp[t].lchild==i)  hcp[i].code[length]=0+'0';
-			if(htp[t].rchild==i)  hcp[i].code[length]=1+'0';
+		if(ht[j].parent!=-1)	{
+			t=ht[j].parent;
+			if(ht[t].lchild==i)  hc[i].code[length]=0+'0';
+			if(ht[t].rchild==i)  hc[i].code[length]=1+'0';
 			length++;
 			j=i=t;
 		}
 		else
-			hcp[i].code[length]='\n';
+			hc[i].code[length]='\n';
 			
-	reverse(hcp[i].code);	
+	reverse(hc[i].code);	
 	}
+	printf("this is a test in huffcode function\n");
 }
 
 void reverse(char *cp)	{
@@ -122,11 +128,16 @@ void reverse(char *cp)	{
 void main(void)	{
 	int i;
 
-	init(ht,hc);
-	createht(ht);
-	huffcode(ht,hc);
+	printf("this is a test in main function\n");
+	
+	init();
+	createht();
+	huffcode();
 
 	for(i=0;i<n;i++)	{
 		printf("the w is %c,the code is %s\n",hc[i].w,hc[i].code);
 	}
+
+	printf("the n is %d\n",n);	
+	printf("this is a test in main function\n");
 }
